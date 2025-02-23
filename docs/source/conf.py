ae709b42b34cef9e1c609cac218ddc8f59d81376
Alin Marin Elena <alin@elena.re>
2025-02-23 19:40:05 +0000

    tag 0.0.28

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,3 +28,78 @@ exclude_patterns = []
 html_theme = "sphinx_immaterial"
 
 html_static_path = ['_static']
+
+# material theme options (see theme.conf for more information)
+html_theme_options = {
+    "icon": {
+        "repo": "fontawesome/brands/github",
+        "edit": "material/file-edit-outline",
+    },
+    "site_url": "https://ddmms.github.io/pack-mm/",
+    "repo_url": "https://github.com/ddmms/pack-mm/",
+    "repo_name": "pack-mm",
+    "edit_uri": "blob/main/docs",
+    "globaltoc_collapse": True,
+    "features": [
+        "navigation.expand",
+        # "navigation.tabs",
+        # "toc.integrate",
+        "navigation.sections",
+        # "navigation.instant",
+        # "header.autohide",
+        "navigation.top",
+        # "navigation.tracking",
+        # "search.highlight",
+        "search.share",
+        "toc.follow",
+        "toc.sticky",
+        "content.tabs.link",
+        "announce.dismiss",
+    ],
+    "palette": [
+        {
+            "media": "(prefers-color-scheme: light)",
+            "scheme": "default",
+            "primary": "light-green",
+            "accent": "light-blue",
+            "toggle": {
+                "icon": "material/lightbulb-outline",
+                "name": "Switch to dark mode",
+            },
+        },
+        {
+            "media": "(prefers-color-scheme: dark)",
+            "scheme": "slate",
+            "primary": "teal",
+            "accent": "lime",
+            "toggle": {
+                "icon": "material/lightbulb",
+                "name": "Switch to light mode",
+            },
+        },
+    ],
+    # BEGIN: version_dropdown
+    "version_dropdown": True,
+    "version_info": [
+        {
+            "version": "https://ddmms.github.io/pack-mm",
+            "title": "Github Pages",
+            "aliases": [],
+        },
+    ],
+    # END: version_dropdown
+    "toc_title_is_page_title": True,
+    # BEGIN: social icons
+    "social": [
+        {
+            "icon": "fontawesome/brands/github",
+            "link": "https://github.com/jbms/sphinx-immaterial",
+            "name": "Source on github.com",
+        },
+        {
+            "icon": "fontawesome/brands/python",
+            "link": "https://pypi.org/project/sphinx-immaterial/",
+        },
+    ],
+    # END: social icons
+}
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -6,10 +6,8 @@
 pack-mm documentation
 =====================
 
-tutorials/index
-
-
-
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+
+  tutorials/index.rst
